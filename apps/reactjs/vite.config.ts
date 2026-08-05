6ee1378cbb5fafff250c66b192ee49037996f056
chlbri <51156116+chlbri@users.noreply.github.com>
2026-08-05 01:50:17 +0000

    refactor: remove vite-tsconfig-paths, clean up package exports, and update SSR configuration in apps
    _NO_CI

--- apps/reactjs/vite.config.ts
+++ apps/reactjs/vite.config.ts
@@ -2,12 +2,12 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 const config = defineConfig({
-  resolve: { tsconfigPaths: true, dedupe: ['react', 'react-dom'] },
   server: { port: 3022 },
-  plugins: [tsconfigPaths(), tanstackStart(), nitro(), viteReact()],
+  resolve: { tsconfigPaths: true, dedupe: ['react', 'react-dom'] },
+  ssr: { noExternal: ['@bemedev/subscriber'] },
+  plugins: [tanstackStart(), nitro(), viteReact()],
 });
 
 export default config;
--- apps/solidjs/vite.config.ts
+++ apps/solidjs/vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 import { tanstackStart } from '@tanstack/solid-start/plugin/vite';
 import { nitro } from 'nitro/vite';
 import viteSolid from 'vite-plugin-solid';
 
 export default defineConfig({
-  resolve: { tsconfigPaths: true },
   server: { port: 3021 },
+  resolve: { tsconfigPaths: true },
+  ssr: { noExternal: ['@bemedev/subscriber'] },
   plugins: [
-    tsconfigPaths(),
     tanstackStart({}),
     nitro(),
     viteSolid({ ssr: true, extensions: ['.js', '.ts', '.jsx', '.tsx'] }),
