9902b06a57eccfb600a4e79c08401cd0718d9b96
chlbri <51156116+chlbri@users.noreply.github.com>
2026-08-05 01:20:29 +0000

    _NO_CI

--- apps/reactjs/vite.config.ts
+++ apps/reactjs/vite.config.ts
@@ -1,10 +1,18 @@
+import { fileURLToPath } from 'node:url';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
 
 const config = defineConfig({
-  resolve: { tsconfigPaths: true, dedupe: ['react', 'react-dom'] },
+  resolve: {
+    alias: {
+      '#': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+    tsconfigPaths: true,
+    dedupe: ['react', 'react-dom'],
+  },
   server: { port: 3022 },
   plugins: [tanstackStart(), nitro(), viteReact()],
 });
